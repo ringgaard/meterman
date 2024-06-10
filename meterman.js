724e472964d2c05860f76c0b3c642f0caf20fcf6
Michael Ringgaard <michael@ringgaard.com>
2024-06-10 19:28:40 +0200

    Initial commit

---
+++ meterman.js
@@ -0,0 +1,436 @@
+import {Component} from "/common/lib/component.js";
+import {MdApp, MdCard, MdDialog, StdDialog} from "/common/lib/material.js";
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+const medium_table = {
+  0: "Other",
+  1: "Oil",
+  2: "Electricity",
+  3: "Gas",
+  4: "Heat",
+  5: "Steam",
+  6: "Hot Water",
+  7: "Water",
+  8: "H.C.A.",
+}
+
+function pad2(num) {
+  return ("0" + num).toString().slice(-2)
+}
+
+function pad4(num) {
+  return ("000" + num).toString().slice(-4)
+}
+
+function datestr(ts) {
+  if (!ts) return "";
+  let date = new Date(ts * 1000);
+  let year = pad4(date.getFullYear());
+  let month = pad2(date.getMonth() + 1);
+  let day = pad2(date.getDate());
+  let hours = pad2(date.getHours());
+  let mins = pad2(date.getMinutes());
+  let secs = pad2(date.getSeconds());
+  return `${year}-${month}-${day} ${hours}:${mins}:${secs}`;
+}
+
+class MeterApp extends MdApp {
+  onconnected() {
+    this.monitor_updates();
+    this.attach(this.ondownload, "click", "#download");
+  }
+
+  ondownload(e) {
+    let url = this.database?.software;
+    if (url) window.open(url);
+  }
+
+  async monitor_updates() {
+    let seq = 0;
+    while (true) {
+      let r = await fetch(`/meterman/state?seq=${seq}`);
+      if (r.status == 304) {
+        continue;
+      } else if (r.status == 200) {
+        let state = await r.json();
+        if (state.seq != seq) {
+          seq = state.seq;
+          this.database_update(state);
+        }
+      } else {
+        await delay(30000);
+      }
+    }
+  }
+
+  select(gwid) {
+    this.current = gwid;
+    let gw = this.current && this.database.gateways[this.current];
+    this.find("#gateway").update(gw);
+  }
+
+  database_update(state) {
+    this.database = state;
+    this.find("#gateways").update(state);
+    let gw = this.current && state.gateways[this.current];
+    if (!gw) this.current = undefined;
+    this.find("#gateway").update(gw);
+  }
+
+  render() {
+    return `
+      <md-toolbar>
+        <md-toolbar-logo></md-toolbar-logo>
+        <div id="title">Meter Manager</div>
+        <md-spacer></md-spacer>
+        <md-icon-button id="download" icon="download"></md-icon-button>
+      </md-toolbar>
+
+      <md-content>
+        <div id="cols">
+          <meter-gateways id="gateways"></meter-gateways>
+          <meter-gateway id="gateway"></meter-gateway>
+        </div>
+      </md-content>
+    `;
+  }
+
+  static stylesheet() {
+    return `
+      $ #cols {
+        display: flex;
+        font-size: 16px;
+      }
+    `;
+  }
+}
+
+Component.register(MeterApp);
+
+class MeterGateways extends MdCard {
+  onconnect() {
+    this.attach(this.onclick, "click");
+  }
+
+  onclick(e) {
+    let gwid = e.target.getAttribute("gw");
+    if (gwid) this.match("meter-app").select(gwid);
+  }
+
+  render() {
+    let h = new Array();
+    h.push("<md-card-toolbar>Gateways</md-card-toolbar>");
+    let gateways = this.state && this.state.gateways;
+    if (gateways) {
+      for (let gw of Object.values(gateways)) {
+        h.push(`<div class="entry" gw="${gw.gw}">${gw.gw}</div>`);
+      }
+    }
+    return h.join("");
+  }
+
+  static stylesheet() {
+    return `
+      $ {
+        min-width: 200px;
+      }
+      $ .entry {
+        padding: 4px;
+      }
+      $ .entry:hover {
+        background-color: #eeeeee;
+        cursor: pointer;
+      }
+    `;
+  }
+}
+
+Component.register(MeterGateways);
+
+class MeterGateway extends MdCard {
+  render() {
+    let gw = this.state;
+    if (!gw) return "";
+    let upsince = datestr(gw.ts);
+    let lastseen = datestr(gw.lastseen);
+
+    let h = new Array();
+    h.push(`<md-card-toolbar>Gateway ${gw.gw}</md-card-toolbar>`);
+
+    h.push('<div>');
+    h.push('<md-button id="reset" label="Reset"></md-button>');
+    h.push('<md-button id="rescan" label="Rescan"></md-button>');
+    h.push('<md-button id="sync" label="Sync"></md-button>');
+    h.push('<md-button id="configure" label="Configure"></md-button>');
+    h.push('<md-button id="upgrade" label="Upgrade"></md-button>');
+    h.push('<md-button id="forget" label="Forget"></md-button>');
+    h.push('<md-button id="command" label="Command"></md-button>');
+    h.push('<md-button id="log" label="Log"></md-button>');
+    h.push('</div>');
+
+    h.push('<div class="info">');
+
+    h.push('<div class="controller">');
+    h.push(`<span class="title">Controller</span>`);
+    h.push(`<table>`);
+    h.push(`<tr><td>App</td><td>${gw.app || ""}</td></tr>`);
+    h.push(`<tr><td>Version</td><td>${gw.version || ""}</td></tr>`);
+    h.push(`<tr><td>Build date</td><td>${gw.build || ""}</td></tr>`);
+    h.push(`<tr><td>Up since</td><td>${upsince}</td></tr>`);
+    h.push(`<tr><td>Last seen</td><td>${lastseen}</td></tr>`);
+    h.push(`<tr><td>Contol topic</td><td>${gw.control || ""}</td></tr>`);
+    h.push(`<tr><td>PID</td><td>${gw.pid || ""}</td></tr>`);
+    h.push(`<tr><td>OS</td><td>${gw.os || ""}</td></tr>`);
+    h.push(`<tr><td>Model</td><td>${gw.model || ""}</td></tr>`);
+    h.push(`<tr><td>Revision</td><td>${gw.revision || ""}</td></tr>`);
+    h.push(`<tr><td>Serial no.</td><td>${gw.serial || ""}</td></tr>`);
+    h.push(`</table>`);
+
+    h.push('</div>');
+
+    h.push('<div class="configuration">');
+    h.push(`<span class="title">Configuration</span>`);
+    h.push(`<pre>${gw.config || ""}</pre>`);
+    h.push('</div>');
+
+    h.push('</div>');
+
+    h.push('</div>');
+
+    h.push(`
+      <div class="meters">
+        <span class="title">Meters</span>
+        <md-data-table id="metertab">
+          <md-data-field field="meterid">Meter ID</md-data-field>
+          <md-data-field field="manufacturer">Manufacturer</md-data-field>
+          <md-data-field field="type">Type</md-data-field>
+          <md-data-field field="version">Version</md-data-field>
+          <md-data-field field="bus">Bus</md-data-field>
+          <md-data-field field="address">Address</md-data-field>
+          <md-data-field field="reading" style="text-align: right">Reading</md-data-field>
+          <md-data-field field="time">Time</md-data-field>
+        </md-data-table>
+      </div>
+    `);
+
+    if (gw.console && gw.console.trim().length > 0) {
+      h.push(`
+        <div class="console">
+          <span class="title">Console</span>
+          <pre>${gw.console || ""}</pre>
+        </div>
+      `);
+    }
+
+    return h.join("");
+  }
+
+  onrendered() {
+    if (!this.state) return;
+
+    let meters = this.state.meters;
+    if (meters) {
+      let rows = new Array();
+      for (let m of Object.values(meters)) {
+        let type = m.type || m.medium;
+        type = medium_table[type] || type || "";
+
+        let reading = m.value;
+        if (reading && m.unit) {
+          reading = `${reading} ${m.unit}`;
+        }
+        if (!reading && m.reading) {
+          let r = m.reading[0];
+          reading = r.value;
+          if (r.unit) reading = `${reading} ${r.unit}`;
+        }
+
+        let time = datestr(m.ts);
+        rows.push({
+          meterid: m.meterid,
+          manufacturer: m.manufacturer,
+          type: type,
+          version: m.version,
+          bus: `${m.bus || ""} ${m.device || ""}`,
+          address: m.address,
+          reading: reading,
+          time:time,
+        });
+      }
+      this.find("#metertab").update(rows);
+    }
+
+    this.attach(this.onreset, "click", "#reset");
+    this.attach(this.onsync, "click", "#sync");
+    this.attach(this.onupgrade, "click", "#upgrade");
+    this.attach(this.onforget, "click", "#forget");
+    this.attach(this.oncommand, "click", "#command");
+    this.attach(this.onconfigure, "click", "#configure");
+    this.attach(this.onrescan, "click", "#rescan");
+    this.attach(this.onlog, "click", "#log");
+  }
+
+  async onreset(e) {
+    let ok = await StdDialog.ask(
+      "Reset", `Restart gateway ${this.state.gw}?`);
+    if (ok) await this.command("reset");
+  }
+
+  async onlog(e) {
+    await this.command("log");
+  }
+
+  async onsync(e) {
+    let ok = await StdDialog.ask(
+      "Sync", `Synchronze time on gateway ${this.state.gw}?`);
+    if (ok) await this.command("timesync");
+  }
+
+  async onrescan(e) {
+    let ok = await StdDialog.ask(
+      "Sync", `Rescan for new meters on gateway ${this.state.gw}?`);
+    if (ok) await this.command("rescan");
+  }
+
+  async onupgrade(e) {
+    let ok = await StdDialog.ask(
+      "Upgrade", `Upgrade software on gateway ${this.state.gw}?`);
+    if (ok) await this.command("upgrade");
+  }
+
+  async onforget(e) {
+    let ok = await StdDialog.ask(
+      "Forget", `Forget gateway ${this.state.gw}?`);
+    if (!ok) return;
+    if (ok) await this.command("forget");
+  }
+
+  async oncommand(e) {
+    let command = await StdDialog.prompt(
+      `Send command to gateway ${this.state.gw}?`, "Command");
+    if (command) await this.command("command", command);
+  }
+
+  async onconfigure(e) {
+    let dialog = new ConfigDialog(this.state.config);
+    let config = await dialog.show();
+    if (config) await this.command("config", config);
+  }
+
+  async command(cmd, body) {
+    let options = {method: "POST"};
+    if (body) {
+      options.body = body;
+      options.headers = {"Content-Type": "text/plain"};
+    }
+    let r = await fetch(`/meterman/${cmd}?gw=${this.state.gw}`, options);
+    if (!r.ok) console.log("Error", r);
+  }
+
+  static stylesheet() {
+    return `
+      $ {
+        width: 100%;
+      }
+      $ .title {
+        display:inline-block;
+        font-size: 18px;
+        font-weight: bold;
+        padding: 12px 0 12px 4px;
+      }
+      $ .info {
+        display: flex;
+        flex-direction: row;
+        gap: 4px;
+        padding: 8px 0 8px 0;
+      }
+      $ .controller {
+        flex: 1;
+        padding: 8px;
+        border: 2px solid #dddddd;
+        font-size: 16px;
+      }
+      $ .configuration {
+        flex: 1;
+        padding: 4px;
+        border: 2px solid #dddddd;
+      }
+      $ .configuration pre {
+        font-size: 11px;
+        margin: 2px;
+        white-space: pre-wrap;
+       }
+      $ .controller td {
+        padding: 8px 16px 0 0;
+      }
+      $ .meters {
+        padding: 4px;
+        border: 2px solid #dddddd;
+      }
+      $ .console {
+        flex: 1;
+        margin-top: 8px;
+        padding: 4px 4px 4px;
+        border: 2px solid #dddddd;
+      }
+
+      $ .console pre {
+        font-size: 11px;
+        white-space: pre-wrap;
+      }
+    `;
+  }
+}
+
+Component.register(MeterGateway);
+
+class ConfigDialog extends MdDialog {
+  onconnected() {
+    this.bind("textarea", "keydown", e => e.stopPropagation());
+  }
+
+  submit() {
+    let config = this.find("textarea").value;
+    try {
+      JSON.parse(config);
+    } catch(e) {
+      this.find("#msg").innerText = e.message;
+      return;
+    }
+    this.close(config);
+  }
+
+  render() {
+    let config = this.state;
+    return `
+      <md-dialog-top>Configure gateway</md-dialog-top>
+      <div id="content">
+        <textarea
+          rows="32"
+          cols="80"
+          spellcheck="false">${Component.escape(config)}</textarea>
+          <div id="msg"></div>
+      </div>
+      <md-dialog-bottom>
+        <button id="cancel">Cancel</button>
+        <button id="submit">Configure</button>
+      </md-dialog-bottom>
+    `;
+  }
+
+  static stylesheet() {
+    return `
+      $ #content {
+        display: flex;
+        flex-direction: column;
+        row-gap: 16px;
+      }
+    `;
+  }
+}
+
+Component.register(ConfigDialog);
+
+document.body.style = null;
+
---
+++ meterman.py
@@ -0,0 +1,269 @@
+# Copyright 2021 Ringgaard Research ApS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import time
+import paho.mqtt.client as mqtt
+import threading
+
+import sling
+import sling.flags as flags
+import sling.log as log
+import sling.net
+
+flags.define("--port",
+             help="port number for the HTTP server",
+             default=8080,
+             type=int,
+             metavar="PORT")
+
+flags.define("--mqtt",
+             help="MQTT server",
+             default="vault.ringgaard.com",
+             metavar="HOST")
+
+flags.define("--history",
+             help="Keep history of meter readings",
+             default=False,
+             action="store_true")
+
+flags.define("--appdir",
+             help="Application directory for upgrades",
+             default="/var/data/metermon",
+             metavar="PATH")
+
+# Parse command line flags.
+flags.parse()
+
+# Initialize web server.
+app = sling.net.HTTPServer(flags.arg.port)
+app.static("/common", "app", internal=True)
+app.redirect("/", "/meterman/")
+
+# Main page.
+app.page("/meterman",
+"""
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name=viewport content="width=device-width, initial-scale=1">
+  <title>meterman</title>
+  <link rel="icon" href="/common/image/appicon.ico" type="image/x-icon" />
+  <script type="module" src="/meterman/meterman.js"></script>
+</head>
+<body style="display: none">
+  <meter-app id="app">
+  </my-app>
+</body>
+</html>
+""")
+
+app.file("/meterman/meterman.js", "meterman.js", "text/javascript")
+
+# Global state.
+next_seq = 1
+state = {
+  "seq": 0,
+  "software": "/meterman/download/metermon",
+  "gateways": {},
+}
+
+gateways = state["gateways"]
+
+state_upate = threading.Event()
+
+def state_updated():
+  global next_seq
+  state["seq"] = next_seq
+  next_seq += 1
+  state_upate.set()
+  state_upate.clear()
+  #print(json.dumps(state, indent=2))
+
+@app.route("/meterman/state", method="GET")
+def state_request(request):
+  seq = int(request.param("seq"))
+
+  # Wait for state update if client already has the current version.
+  if seq == state["seq"]: state_upate.wait(30)
+
+  # Return "Not modified" if no changes
+  if seq == state["seq"]: return 304
+
+  # Return new state.
+  return state
+
+def get_gateway(gwid):
+  gw = gateways.get(gwid)
+  if gw is None:
+    gw = {"gw": gwid, "meters":{}}
+    gateways[gwid] = gw
+  return gw
+
+# Subscribe to meter messages on MQTT connect
+def on_mqtt_connect(client, userdata, flags, reason_code, properties):
+  # Subscribe to messages from meter gateways.
+  client.subscribe("meter/data/#")
+
+# Handle meter messages and update global state.
+def on_mqtt_message(client, userdata, msg):
+  #print("message", msg.topic, msg.payload)
+  try:
+    message = json.loads(msg.payload)
+  except Exception as e:
+    print("invalid JSON message:", msg.payload)
+    return
+  print("message:", message)
+
+  op = message.get("op")
+  if op is None:
+    print("invalid message: missing op")
+    return
+
+  gwid = message.get("gw")
+  if gwid is None:
+    print("invalid message: missing gateway id")
+    return
+
+  if op == "startup":
+    print("startup", gwid)
+    gw = get_gateway(gwid)
+    gw["lastseen"] = message["ts"]
+    gw.update(message)
+    state_updated()
+  elif op == "inventory":
+    print("inventory", gwid)
+    gw = get_gateway(gwid)
+    gw["lastseen"] = message["ts"]
+    meters = gw["meters"]
+    for m in message["meters"]:
+      meterid = m["meterid"]
+      meter = meters.get(meterid)
+      if meter is None:
+        meter = {"readings": []}
+        meters[meterid] = meter
+      meter.update(m)
+    state_updated()
+  elif op == "reading":
+    print("reading", gwid)
+    gw = get_gateway(gwid)
+    gw["lastseen"] = message["ts"]
+    meters = gw["meters"]
+    meterid = message.get("meterid")
+    if meterid is None:
+      print("missing meter id in reading")
+      return
+    meter = meters.get(meterid)
+    if meter is None:
+      meter = {"readings": []}
+      meters[meterid] = meter
+    reading = message.get("reading")
+    if reading is None or reading[0]["vif"] != 255:
+      meter.update(message)
+    if flags.arg.history: meter["readings"].append(message)
+    state_updated()
+  elif op == "console":
+    gw = get_gateway(gwid)
+    console = message["console"]
+    gw["console"] = console
+    state_updated()
+  else:
+    print("unknown message type:", op, "from", gwid)
+
+# Start MQTT client.
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_connect = on_mqtt_connect
+mqttc.on_message = on_mqtt_message
+mqttc.connect(flags.arg.mqtt, 1883, 60)
+
+@app.route("/meterman/reset", method="POST")
+def reset_request(request):
+  gwid = request.param("gw")
+  gw = gateways.get(gwid)
+  if gw is None: return 404
+  mqttc.publish(gw["control"], json.dumps({"op": "reset"}))
+
+@app.route("/meterman/timesync", method="POST")
+def reset_request(request):
+  gwid = request.param("gw")
+  gw = gateways.get(gwid)
+  if gw is None: return 404
+  ts = int(time.time())
+  mqttc.publish(gw["control"], json.dumps({"op": "timesync", "ts": ts}))
+
+@app.route("/meterman/upgrade", method="POST")
+def upgrade_request(request):
+  gwid = request.param("gw")
+  gw = gateways.get(gwid)
+  if gw is None: return 404
+
+  f = open(flags.arg.appdir + "/metermon", "rb")
+  data = f.read();
+  f.close()
+
+  print("upgrade", gwid, len(data), type(data))
+  msg = {"op": "upgrade", "binary": data.hex()}
+  mqttc.publish(gw["control"], json.dumps(msg))
+
+@app.route("/meterman/forget", method="POST")
+def forget_request(request):
+  gwid = request.param("gw")
+  del gateways[gwid]
+  state_updated()
+
+@app.route("/meterman/command", method="POST")
+def command_request(request):
+  gwid = request.param("gw")
+  command = request.body.decode()
+  gw = gateways.get(gwid)
+  if gw is None: return 404
+  msg = {"op": "command", "command": command}
+  mqttc.publish(gw["control"], json.dumps(msg))
+
+@app.route("/meterman/config", method="POST")
+def config_request(request):
+  gwid = request.param("gw")
+  config = request.body.decode()
+  gw = gateways.get(gwid)
+  if gw is None: return 404
+  print("config:", config)
+
+  msg = {"op": "config", "config": config}
+  mqttc.publish(gw["control"], json.dumps(msg))
+
+@app.route("/meterman/rescan", method="POST")
+def rescan_request(request):
+  gwid = request.param("gw")
+  gw = gateways.get(gwid)
+  if gw is None: return 404
+  mqttc.publish(gw["control"], json.dumps({"op": "rescan"}))
+
+@app.route("/meterman/log", method="POST")
+def log_request(request):
+  gwid = request.param("gw")
+  gw = gateways.get(gwid)
+  if gw is None: return 404
+  mqttc.publish(gw["control"], json.dumps({"op": "log"}))
+
+@app.route("/meterman/download")
+def down_request(request):
+  app = flags.arg.appdir + "/metermon"
+  return sling.net.HTTPFile(app, "application/octet-stream")
+
+# Run app until shutdown.
+log.info("running")
+app.start()
+mqttc.loop_forever()
+
